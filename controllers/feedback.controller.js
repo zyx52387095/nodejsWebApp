80088f035fadea8f5477683222d67d98c1986323
zyx52387095 <zyx52387095@hotmail.com>
2018-03-17 23:02:50 -0400

    feedback.home

--- controllers/feedback.controller.js
+++ controllers/feedback.controller.js
@@ -21,21 +21,21 @@ const updt = function (req, res) {
 
 const update = function(req, res){
     if(req.body.feedback){
-        var currentUser = req.session.username;
+        var currentUser = req.session.user.username;
         var newFeedback = req.body.feedback;    
     const ret = {};
-    var currentUser = req.session.username;
-    var newFeedback = req.body.feedback;
     feedbacks.findOneAndUpdate({username:currentUser},{$set:{feedback:newFeedback}},function(err,feedback){
         if (err) {
             ret.msg = err.message;
         } else {
             ret.msg = "feedback submitted"
         }
-        //res.render("thankyou", { data: ret });
-        res.redirect('/feedback/thankyou');
+        res.render("thankyou", { fb: newFeedback });
+        //res.redirect('/feedback/thankyou');
     });
-    }}
+}}
+
+
 
 const home = function (req, res) {
     const ret = {};
